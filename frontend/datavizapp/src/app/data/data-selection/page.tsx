1c6225f973011205b91433d89c340af20aef80ef
jche0375 <jche0375@student.monash.edu>
2025-07-28 23:45:27 +0800

    batch migration - frontend

--- frontend/datavizapp/src/app/data/data-selection/page.tsx
+++ frontend/datavizapp/src/app/data/data-selection/page.tsx
@@ -1,14 +1,17 @@
-// import { getWeatherForecast } from "@/app/api/data/data-selection"
-// export default async function DatasetSelectionPage() {
-//   const weatherData = await getWeatherForecast();
-//   return (
-//     <div>
-//       <h1 className="text-2xl font-bold">Weather Forecast</h1>
-//       <ul>
-//         {weatherData.map((item, idx) => (
-//           <li key={idx}>{JSON.stringify(item)}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
-// }
+import { getWeatherForecast } from "@/app/api/data/data-selection"
+export default async function DatasetSelectionPage() {
+  // const weatherData = await getWeatherForecast();
+  const weatherData = [
+    { date: "2023-10-01", temperature: 20, condition: "Sunny" }
+  ]
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Weather Forecast</h1>
+      <ul>
+        {weatherData.map((item, idx) => (
+          <li key={idx}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
