bc220330a7a8692b97d18be316ea66d443ea2a9a
jche0375 <jche0375@student.monash.edu>
2025-07-28 23:49:36 +0800

    batch migration - frontend

--- frontend/datavizapp/src/app/api/data/data-selection.ts
+++ frontend/datavizapp/src/app/api/data/data-selection.ts
@@ -1,14 +1,14 @@
-import { BACKEND_URL } from "@/constants/urls"
-import fetchData from "../fetchData";
+// import { BACKEND_URL } from "@/constants/urls"
+// import fetchData from "../fetchData";
 
-export async function getWeatherForecast(): Promise<WeatherData[]> {
-  return await fetchData(BACKEND_URL + '/weatherforecast');
-}
+// export async function getWeatherForecast(): Promise<WeatherData[]> {
+//   return await fetchData(BACKEND_URL + '/weatherforecast');
+// }
 
-export type WeatherData = {
-    "date": string,
-    "temperatureC": number,
-    "summary": "string",
-    "temperatureF": number
-}
+// export type WeatherData = {
+//     "date": string,
+//     "temperatureC": number,
+//     "summary": "string",
+//     "temperatureF": number
+// }
 
