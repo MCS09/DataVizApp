20836c1ad89ef371151739df74cd905792bedc99
jche0375 <jche0375@student.monash.edu>
2025-07-31 11:55:08 +0800

    update test controller

--- backend/DataVizApp/Controllers/Test/TestController.cs
+++ backend/DataVizApp/Controllers/Test/TestController.cs
@@ -24,6 +24,23 @@ public class WeatherController : ControllerBase
             }
         );
 
-        return Ok(forecast);
+        WeatherData[] weatherData =
+        [
+            new WeatherData("2023-10-01", 20, "Sunny", 68),
+            new WeatherData("2023-10-02", 22, "Cloudy", 72),
+            new WeatherData("2023-10-03", 18, "Rainy", 64),
+            new WeatherData("2023-10-04", 25, "Windy", 77),
+            new WeatherData("2023-10-05", 30, "Stormy", 86)
+        ];
+
+        return Ok(weatherData);
     }
+}
+
+record WeatherData(string Date, int TemperatureC, string Summary, int TemperatureF)
+{
+    public string Date { get; set; } = Date;
+    public int TemperatureC { get; set; } = TemperatureC;
+    public string Summary { get; set; } = Summary;
+    public int TemperatureF { get; set; } = TemperatureF;
 }
