fd4a49c7d557cac4f156b1502c08aef53770b052
jche0375 <jche0375@student.monash.edu>
2025-08-03 00:59:05 +0800

    create functions for dynamic python lib functions executions

--- backend/datavizapp-py-functions/function_app.py
+++ backend/datavizapp-py-functions/function_app.py
@@ -1,25 +1,34 @@
 import azure.functions as func
 import logging
+import numpy as np
+import pandas as pd
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+@app.route(route="execute_numpy_function")
+def execute_numpy_function(func_name: str, args=None, kwargs=None):
+    """
+    Dynamically execute a function from numpy.
+    """
+    func = getattr(np, func_name, None)
+    if func is None:
+        raise AttributeError(f"Function '{func_name}' not found in  numpy.")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    args = args or []
+    kwargs = kwargs or {}
+    
+    return func(*args, **kwargs)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+@app.route(route="execute_pandas_function")
+def execute_pandas_function(func_name: str, args=None, kwargs=None):
+    """
+    Dynamically execute a function from pandas.
+    """
+    func = getattr(pd, func_name, None)
+    if func is None:
+        raise AttributeError(f"Function '{func_name}' not found in pandas.")
+
+    args = args or []
+    kwargs = kwargs or {}
+    
+    return func(*args, **kwargs)
